6b3032181ba735d1cc27ca0873ad827e2fd7db1f
6ix9ine4our20wenty <pn12156@hotelplan.net>
2019-09-30 21:43:22 +0200

    initial Commit

---
+++ jokebook/src/test/java/ch/liluna/jokebook/JokebookApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.liluna.jokebook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JokebookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
