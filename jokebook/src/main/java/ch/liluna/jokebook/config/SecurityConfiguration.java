65448a8cef654ba789b9fb1d99c91208fb5ba242
6ix9ine4our20wenty <pn12156@hotelplan.net>
2019-10-02 10:49:30 +0200

    Adding Config Files

---
+++ jokebook/src/main/java/ch/liluna/jokebook/config/SecurityConfiguration.java
@@ -0,0 +1,62 @@
+package ch.liluna.jokebook.config;
+
+import ch.liluna.jokebook.domain.User;
+import ch.liluna.jokebook.filter.JwtAuthenticationFilter;
+import ch.liluna.jokebook.filter.JwtAuthorizationFilter;
+import ch.liluna.jokebook.service.UserService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UserService userService;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable().authorizeRequests().anyRequest().authenticated().and()
+				.addFilter(new JwtAuthenticationFilter(authenticationManager()))
+				.addFilter(new JwtAuthorizationFilter(authenticationManager())).sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		List<User> users = userService.findAll();
+
+		for (User user : users) {
+			auth.inMemoryAuthentication().withUser(user.getUsername())
+					.password(passwordEncoder().encode(user.getPassword()))
+					.authorities(user.getRoleIDFS().getRoleName());
+		}
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+
+		return source;
+	}
+}
